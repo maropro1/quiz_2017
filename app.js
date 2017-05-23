063033f4a326b58b0da9f1a0e4ebebcbfe57cb9e
maropro1 <fenakito@hotmail.com>
2017-05-23 13:17:48 +0200

    2

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(partials());
-app.use(methodOverride('_method', {methods: ["POST", "GET"]});
+app.use(methodOverride('_method', {methods: ["POST", "GET"]}));
 app.use('/', index);
 
 // catch 404 and forward to error handler
