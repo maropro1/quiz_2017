d1a7793a3bbee06c1a50bbe3b189f082c72b62af
maropro1 <fenakito@hotmail.com>
2017-05-23 11:04:29 +0200

    2

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -188,7 +188,12 @@ exports.check = function (req, res, next) {
     });
 };
 
-exports.random_play  = function (req, res, next) {
+exports.random_play = function (req, res, next) {
 	var score = 0;
 	
-}
+	res.render('quizzes/random_play', {
+        quiz: req.quiz,
+        score: score,
+    });
+	
+};
