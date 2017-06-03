bbba0c55ac1ac9ac007cf3b0e0b86e0cf7ffde18
maropro1 <fenakito@hotmail.com>
2017-06-03 11:50:04 +0200

    9

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -191,8 +191,9 @@ exports.check = function (req, res, next) {
 exports.random_play = function (req, res, next) {
 
     var score = 0;
+	quiz = Quiz.findOne();
     res.render('quizzes/random_play', {
-        quiz: req.quiz,
+        quiz: quiz,
         score: score     
     });
 };
