0584816f4874464bf33b29d5afaa898e981ff6b6
maropro1 <fenakito@hotmail.com>
2017-06-06 21:15:16 +0200

    4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -6,9 +6,9 @@ var paginate = require('../helpers/paginate').paginate;
 // Autoload el quiz asociado a :quizId
 exports.load = function (req, res, next, quizId) {
 
-    models.Quiz.findById(quizId, {
+models.Quiz.findById(quizId, {
     include: [
-        {model: models.Tip, include: [{model: models.User, as: 'Author'}]},
+        {model: models.Tip},
         {model: models.User, as: 'Author'}
     ]
 })
